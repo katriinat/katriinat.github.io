033599706f90d637ce5834c5fe9424e868ac211e
katriina.tiainen@myy.haaga-helia.fi <katriina.tiainen@myy.haaga-helia.fi>
2024-09-28 18:16:09 +0300

    Korjauksia

--- oma2.js
+++ oma2.js
@@ -28,13 +28,13 @@ function kerro(data){
     // Muiden tietojen käsittely
     teksti = teksti + "<p>" + data.kuvaus + "</p>";
     teksti = teksti + "<p><img src='" + data.kuva + "' alt='Työntekijöitä Rautakonttorin tietokonekeskuksessa Rautatalossa' ></p>";
-    teksti = teksti + "<h3>" + data.toteutus.nimi + " " + data.toteutus.tunnus + " " + data.toteutus.opintopisteet + " op" + "</h3>";
-    teksti = teksti + "<p>Toteutuksen ajankohta: " + data.alkupvm + "-" + data.loppupvm + "</p>";
-    teksti = teksti + "<p>Toteutuksen kesto: " + data.kesto + " viikkoa" + "</p>";
-    teksti = teksti + "<p>Osallistujien lukumäärä: " + data.osallistujat_lkm + "</p>";
+    teksti = teksti + "<h3>" + data.toteutus.nimi + " " + data.toteutus.tunnus + ", " + data.toteutus.opintopisteet + " op" + "</h3>";
+    teksti = teksti + "<p>Toteutuksen ajankohta: " + data.alkupvm + "-" + data.loppupvm + "</br>";
+    teksti = teksti + "Toteutuksen kesto: " + data.kesto + " viikkoa" + "</br>";
+    teksti = teksti + "Osallistujien lukumäärä: " + data.osallistujat_lkm + "</p>";
     teksti = teksti + "<h4>Osallistujat " + "</h4>" + "<ul>"
         for (var i = 0; i < data.osallistujat.length; i++) {
-            teksti = teksti + "<li>" + data.osallistujat[i].etunimi + data.osallistujat[i].sukunimi + "</li>";
+            teksti = teksti + "<li>" + data.osallistujat[i].etunimi + " " + data.osallistujat[i].sukunimi + "</li>";
         }
     teksti = teksti + "</ul>";
 
